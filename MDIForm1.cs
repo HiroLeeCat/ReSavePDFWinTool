048d31e14e85ebf668b9c409cedf76f941389e2d
HiroLeeCat <hirocat@digiwin.com>
2025-03-06 16:00:28 +0800

    增加過濾只處理新建/修改 幾天內的pdf檔

--- MDIForm1.cs
+++ MDIForm1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 namespace ReSavePDFWinTool
@@ -8,16 +10,41 @@ namespace ReSavePDFWinTool
 	{
         #region 全域變數
         string gMsg = string.Empty;
+        //2025/03/06;1.0.0.1;僅檢查最近7天內新增/修改的pdf檔
+        int gLastModifiedDays = 0;
         #endregion 全域變數
                 
         public MDIForm1()
 		{
 			InitializeComponent();
             this.Text = string.Format("檢查異常PDF讀取失敗，則嘗試將PDF執行另存 工具 (Ver:{0})", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            //2025/03/06;1.0.0.1;僅檢查最近7天內新增/修改的pdf檔↓
+            //過濾新增/修改幾天內的檔案
+            string strParameterKey = "LastModifiedDays";
+            string strParameterValue = "";//從config取得參數值
+            try
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                //獲取appSettings節點
+                AppSettingsSection app = (AppSettingsSection)config.GetSection("appSettings");
+                strParameterValue = app.Settings[strParameterKey].Value;
+            }
+            catch
+            {
+                //找不到系統參數就預設回傳空白
+                strParameterValue = "";
+            }
+            if (strParameterValue.Trim().Length > 0)
+            {
+                int.TryParse(strParameterValue, out gLastModifiedDays);
+            }
+            //2025/03/06;1.0.0.1;僅檢查最近7天內新增/修改的pdf檔↑
         }
 
-		#region 點選執行檢查
-		private void btnReSavePDF_Click(object sender, EventArgs e)
+        #region 點選執行檢查
+        private void btnReSavePDF_Click(object sender, EventArgs e)
 		{
             //進度列預設大小為0
             txtResult.Text = "";
@@ -110,11 +137,14 @@ namespace ReSavePDFWinTool
             DirectoryInfo directoryinfo = new DirectoryInfo(pFolderPath);
             if (directoryinfo.Exists)
             {
+                //if (cbxContainSubFolders.Checked)
                 if (tCommon.IsHaveSub)
                 {
+                    //先處理子資料夾
                     DirectoryInfo[] dirSubs = directoryinfo.GetDirectories();
                     foreach (DirectoryInfo SubDir in dirSubs)
                     {
+                        //跳過用來備份原pdf的bakPDF資料夾，裡頭的pdf都是異常檔案，就不用進去檢查了
                         if (SubDir.Name.IndexOf("bakPDF") == 0)
                             continue;
                         string tempPath = System.IO.Path.Combine(pFolderPath, SubDir.Name);
@@ -122,6 +152,10 @@ namespace ReSavePDFWinTool
                     }
                 }
 
+                // 取得所有 PDF 檔案
+                //string[] files = Directory.GetFiles(pFolderPath, "*.pdf", SearchOption.AllDirectories);
+
+                //FileInfo[] aryFileInfo = directoryinfo.GetFiles();
                 FileInfo[] aryFileInfo = directoryinfo.GetFiles();
                 int intFileCount = aryFileInfo.Length;
                 int intPDFFileNum = 0;
@@ -137,6 +171,7 @@ namespace ReSavePDFWinTool
 
                     if (intPDFFileNum > 0)
                     {
+                        //gMsg = string.Format("檢查資料夾路徑 {0} start\r\n", pFolderPath);
                         gMsg = string.Format("檢查資料夾路徑：{0} \r\n", pFolderPath);
                         txtResult.Text += gMsg;
 
@@ -147,18 +182,44 @@ namespace ReSavePDFWinTool
                         {
                             if (aryFileInfo[i].ToString().ToLower().EndsWith(".pdf"))
                             {
-                            	CheckPDFInFile(aryFileInfo[i]);
+                                //2025/03/06;1.0.0.1;僅檢查最近7天內新增/修改的pdf檔↓
+                                if (gLastModifiedDays != 0)
+                                {
+                                    DateTime sevenDaysAgo = DateTime.Now.AddDays(-gLastModifiedDays);
+                                    // 檢查檔案是否在最近幾天內新增或修改過
+                                    if (aryFileInfo[i].CreationTime >= sevenDaysAgo || aryFileInfo[i].LastWriteTime >= sevenDaysAgo)
+                                    {
+                                        //recentPdfFiles.Add(file);
+                                    }
+                                    else
+                                    {
+                                        ++prsBar.Value;
+                                        continue;
+                                    }
+                                }
+                                //2025/03/06;1.0.0.1;僅檢查最近7天內新增/修改的pdf檔↑
+
+                                CheckPDFInFile(aryFileInfo[i]);
                                 ++prsBar.Value;
                             }
                             Application.DoEvents();
                         }
+
+
+
+
+
+		                //gMsg = string.Format("檢查資料夾路徑 {0} end\r\n", pFolderPath);
+		                //txtResult.Text += gMsg;
                     }
                 }
                 catch (Exception ex)
                 {
                     gMsg = "檢查資料夾路徑下的PDF檔發生錯誤：" + ex.Message + "\r\n";
                     txtResult.Text += gMsg;
-                    MessageBox.Show(gMsg);
+                    //改成不要跳警示視窗，讓它繼續執行即可
+                    //MessageBox.Show(gMsg);
+                    WriteLog(gMsg);
                 }
             }
         }
@@ -166,17 +227,25 @@ namespace ReSavePDFWinTool
 
         public void CheckPDFInFile(FileInfo pFile)
         {
+            //gMsg = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") +
+            //"↓開始檢查pdf檔案【" + pFile.FullName + "】↓" + "\r\n";
+	        //txtResult.Text += gMsg;
+            //WriteLog(gMsg);
+
             bool bMustResave = false;
             PdfSharp.Pdf.PdfDocument sharpDoc = new PdfSharp.Pdf.PdfDocument();
             try
             {
+            	//將檢查的pdf檔案，取消「唯讀」屬性
                 File.SetAttributes(pFile.FullName, File.GetAttributes(pFile.FullName) & ~FileAttributes.ReadOnly);
 
+                //部份pdf出現「Object already in table」，解法：請用adobe reader開啟再另存新檔把附件換掉即可
                 sharpDoc = PdfSharp.Pdf.IO.PdfReader.Open(pFile.FullName);//PDF來源檔
+                //txtResult.Text += "PdfShart開啟[" + pTargetFilePath + "]成功，該檔案不需另存新檔！\r\n";
             }
             catch (Exception ex)
             {
-                gMsg = "讀取失敗！filepath=[" + pFile.FullName + "],err=[" + ex.Message + "]\r\n";
+                gMsg = "使用PdfSharp讀取失敗！filepath=[" + pFile.FullName + "],err=[" + ex.Message + "]\r\n";
                 txtResult.Text += gMsg;
                 WriteLog(gMsg);
                 bMustResave = true;
@@ -190,6 +259,11 @@ namespace ReSavePDFWinTool
             {
                 ReSavePDF(pFile);
             }
+
+            //gMsg = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") +
+            //    "↑結束檢查pdf檔案【" + pFile.FullName + "】↑" + "\r\n";
+	        //txtResult.Text += gMsg;
+            //WriteLog(gMsg);
         }
 
         private void ReSavePDF(FileInfo pFile)
@@ -209,6 +283,7 @@ namespace ReSavePDFWinTool
             }
             else
             {
+                //如備份區已存在相同檔名，則另存備份區
                 int iTmp = 1;
                 bool bLoop = true;
                 while (bLoop)
@@ -237,15 +312,15 @@ namespace ReSavePDFWinTool
                     {
                         iText.Kernel.Pdf.PdfDocument itextDoc = new iText.Kernel.Pdf.PdfDocument(reader, writer);
                         itextDoc.Close();
-                        txtResult.Text += "另存pdf成功！\r\n";
-                        WriteLog("另存pdf成功！\r\n");
+                        txtResult.Text += "改用iText開啟並另存pdf成功！\r\n";
+                        WriteLog("改用iText開啟並另存pdf成功！\r\n");
                     }
                 }
             }
             catch (Exception ex3)
             {
-                txtResult.Text += "另存pdf失敗, err=[" + ex3.ToString() + "]\r\n";
-                WriteLog("另存pdf失敗, err=[" + ex3.ToString() + "]\r\n");
+                txtResult.Text += "改用iText開啟並另存pdf失敗, err=[" + ex3.ToString() + "]\r\n";
+                WriteLog("改用iText開啟並另存pdf失敗, err=[" + ex3.ToString() + "]\r\n");
                 File.Move(tBakFilePath, tOriFilePath);//將備份的pdf，再搬回來
             }
         }
@@ -396,7 +471,10 @@ namespace ReSavePDFWinTool
         }
 
 
+		#region 不開啟WinForm工具執行
 		public void RunBatResavePDF() {
+			gMsg = "RunType參數【" + tCommon.RunType + "】, FolderOrFilesPath參數【" + tCommon.FolderOrFilesPath + "】, IsHaveSub參數【" + tCommon.IsHaveSub.ToString() + "】";
+    		WriteLog(gMsg);
 			if((tCommon.RunType!="P" && tCommon.RunType!="F") || tCommon.FolderOrFilesPath==""){
 				gMsg = "參數錯誤，無法執行！";
     			WriteLog(gMsg);
@@ -429,6 +507,7 @@ namespace ReSavePDFWinTool
 	            }
 			}
 		}
+        #endregion 不開啟WinForm工具執行
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -438,10 +517,13 @@ namespace ReSavePDFWinTool
 
     public static class tCommon
 	{
+		//P:資料夾路徑, F:挑選多檔案
 		public static string RunType = "P";
 
+		//資料夾或檔案路徑
 		public static string FolderOrFilesPath = "";
 
+		//是否包含子路徑
 		public static bool IsHaveSub = false;
 	}
 }
